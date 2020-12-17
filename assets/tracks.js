89a4a2a3ec2febe247de02dca3480c6ac7eb1aef
Танана Арина Сергеевна <uit32@maz.local>
2020-12-17 15:45:10 +0300

    Config Css + loading data from DB

--- assets/tracks.js
+++ assets/tracks.js
@@ -1,9 +1,56 @@
+import './styles/app.css';
 import React, {Component} from 'react';
 
 class Tracks extends Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            tracks: [],
+        };
+    }
+
+    componentDidMount() {
+        fetch("http://localhost:8000/tracks")
+            .then(res => res.json())
+            .then(
+                result => {
+                    this.setState({
+                        tracks: [...result]
+                    });
+                },
+                error => {
+                    this.setState({
+                        tracks: []
+                    });
+                }
+            )
+    }
+
     render() {
+        const {tracks} = this.state;
+
         return (
-            <h2>Плейлист</h2>
+            <div>
+                <h2>Плейлист</h2>
+                <table>
+                    <tr>
+                        <th>Исполнитель</th>
+                        <th>Песня</th>
+                        <th>Жанр</th>
+                        <th>Год</th>
+                    </tr>
+                    {tracks.map(track => (
+                        <tr>
+                            <td>{track.singer}</td>
+                            <td>{track.song}</td>
+                            <td>{track.genre}</td>
+                            <td>{track.year}</td>
+                        </tr>
+                    ))}
+                </table>
+
+                <h2>Фильтр</h2>
+            </div>
         );
     }
 }
--- src/Controller/DefaultController.php
+++ src/Controller/DefaultController.php
@@ -2,26 +2,38 @@
 
 namespace App\Controller;
 
+use App\Entity\Track;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
+use function MongoDB\BSON\toJSON;
 
 class DefaultController extends AbstractController
 {
+
+    /**
+     * @Route("/", name="home")
+     */
+    public function index()
+    {
+        return $this->render('default/index.html.twig');
+    }
 
     /**
      * @Route("/tracks", name="tracks")
      */
     public function getTracks(): Response
     {
-
+        $serializer = $this->get('serializer');
+        $repository = $this->getDoctrine()->getRepository(Track::class);
+        $tracks = $serializer->serialize($repository->findAll(), 'json');
 
         $response = new Response();
 
         $response->headers->set('Content-Type', 'application/json');
         $response->headers->set('Access-Control-Allow-Origin', '*');
 
-        $response->setContent(json_encode($tracks));
+        $response->setContent($tracks);
 
         return $response;
     }
